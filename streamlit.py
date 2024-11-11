d3ff716d2a700e62ebf8e3b38dbeea8d2126d931
CarbonRKayd <167077842+CarbonRKayd@users.noreply.github.com>
2024-11-11 13:24:14 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -119,8 +119,8 @@ if Intput_File_new is not None:
 
 
     with st.sidebar:
-        st.markdown(""
-        :red[Select Conveyor from drop down]""
+        st.markdown('''
+        :red[Select Conveyor from drop down]''')
         )
 
     dynamic_filters.display_filters(location='sidebar')
